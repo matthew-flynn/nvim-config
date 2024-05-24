a476b2d15cc9067666859530fa009bb441f25176
Matt Flynn <mmflynn95@gmail.com>
2024-05-24 17:25:31 +0100

    More config changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,41 +1,53 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     opts = {
-      auto_install = true
-    }
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities  = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.tsserver.setup({
-        capabilities = capabilities
-			})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      local powershell_options = {
+        shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+        shellcmdflag =
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+        shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+        shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+        shellquote = "",
+        shellxquote = "",
+      }
+
+      for option, value in pairs(powershell_options) do
+        vim.opt[option] = value
+      end
+
+      local opts = { buffer = 0 }
+
+      vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end,
+  },
+}
