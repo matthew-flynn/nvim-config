54287f1edf52b45799e3606bd7f19c302f628110
Matt Flynn <mmflynn95@gmail.com>
2024-09-04 20:31:56 +0100

    feat: catppuccin transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,10 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    local catppuccin = require("catppuccin")
+    catppuccin.setup({
+      transparent_background = true,
+    })
     vim.cmd.colorscheme("catppuccin")
   end,
 }
